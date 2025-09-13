1397323fd9205de500ee789aa80be714d3453d9a
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-09-13 09:28:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
